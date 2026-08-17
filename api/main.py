c61c82dfd569680145848c0a210e273f22e5cb82
Isaac Jared <L211050222@pabellon.tecnm.mx>
2026-08-17 15:28:29 -0600

    small changes

--- api/main.py
+++ api/main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
-from worker.tasks import image_processing_dummy
+from worker.tasks import image_processing
+from celery.result import AsyncResult
+from worker.tasks import celery_app
 
 app = FastAPI()
 
+# Endpoint to receive an image and send it for processing
 @app.post("/process image/")
 async def get_image(file: UploadFile = File(...)):
     #change later to save the file in disk
     file_name = file.filename
-    task = image_processing_dummy.delay(file_name)
+    task = image_processing.delay(file_name)
     return{
         "message": "Image recived and sent for processing",
         "task_id": task.id,
         "file": file_name
     }
+
+# Endpoint to check the status of a task
+@app.get("/task-state/{tarea_id}")
+async def consultar_estado(tarea_id: str):
+    tarea = AsyncResult(tarea_id, app=celery_app)
+    respuesta = {
+        "tarea_id": tarea_id,
+        "estado": tarea.state, 
+    }
+    if tarea.state == "SUCCESS":
+        respuesta["resultado"] = tarea.result
+        
+    return respuesta        
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,5 +1,9 @@
+from ast import Import
 import time
 from celery import Celery
+import os
+import PIL.Image as Image
+import PIL.ImageEnhance as ImageEnhance
 
 # Initialize Celery app
 celery_app = Celery(
@@ -9,8 +13,50 @@ celery_app = Celery(
 )
 
 @celery_app.task
-def image_processing_dummy(file_name: str):
-    print(f"[{file_name}] Starting image processing")
-    time.sleep(10)  # Simulate a long-running task
-    print(f"[¨{file_name}] Finished image processing")
-    return f"Processed image: {file_name}"
+def image_processing(file_dir: str):
+    print(f"[{file_dir}] Starting image processing")
+    # validate the file does exist
+    if not os.path.exists(file_dir):
+        return{"error": f"File {file_dir} does not exist"}
+    
+    #prepare the file name and route to save the processed image
+    base_name = os.path.basename(file_dir)
+    destination_dir = os.path.join("processed_images", f"autumn_{base_name}")
+    
+    try:
+        with Image.open(file_dir) as img:
+            img = img.convert("RGB")
+            
+            #alter the image to make it look like autumn
+            
+            #increase general contrast
+            enchancer_contrast = ImageEnhance.Contrast(img)
+            img = enchancer_contrast.enhance(1.15)
+            
+            #Increase saturation
+            enchancer_color = ImageEnhance.Color(img)
+            img = enchancer_color.enhance(1.25)
+            
+            #manipulate the color balance to give a warmer tone
+            r, g, b = img.split()
+            r = r.point(lambda i: min(int(i*1.1), 255))
+            b = b.point(lambda i: int(i*0.85))
+            
+            #merge the channels back
+            img = Image.merge("RGB", (r, g, b))  
+            
+            #reduce brightness slightly to give a more autumn feel
+            enhancer_brihtness = ImageEnhance.Brightness(img)
+            final_image = enhancer_brihtness.enhance(0.95)
+            
+            #save the processed image
+            final_image.save(destination_dir, format="JPEG")
+        
+        print(f"[{file_dir}] Finished image processing")
+        return {
+            "message": f"Image processed and saved",
+            "original_file": file_dir,
+            "processed_file": destination_dir
+        }
+    except Exception as e:
+        return {"error": f"failed to process image: {str(e)}"}
